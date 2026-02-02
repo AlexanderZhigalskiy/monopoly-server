d5275febbd10fee7d71c7de623249098c485f143
Aliaksandr Zhyhalski <aliaksandr.zhyhalski@startmatter.com>
2026-02-03 00:27:16 +0300

    12

--- src/main/kotlin/com/monopoly/model/Player.kt
+++ src/main/kotlin/com/monopoly/model/Player.kt
@@ -1,10 +1,21 @@
-package com.monopoly.model
+package com.monopoly
 
-import kotlinx.serialization.Serializable
-
-@Serializable
+// УДАЛИТЕ старый класс и оставьте только это:
 data class Player(
-    val id: Int? = null,
+    val id: Int? = null,  // Сделайте ID nullable
     val name: String,
-    var balance: Int = 1500
-)
+    var balance: Int = 0
+) {
+    fun addMoney(amount: Int) {
+        balance += amount
+    }
+
+    fun subtractMoney(amount: Int): Boolean {
+        return if (balance >= amount) {
+            balance -= amount
+            true
+        } else {
+            false
+        }
+    }
+}
